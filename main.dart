68e38166c033941d65f3a2ae339d9002ddc8e6f2
kugelblitz84 <111196826+kugelblitz84@users.noreply.github.com>
2024-11-30 14:29:26 +0600

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,27 +1,34 @@
+// ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
 import 'package:first_project/pages/homepage.dart';
+//import 'package:first_project/pages/animated_search.dart';
 // import 'package:first_project/pages/signup.dart';
 // import 'package:first_project/pages/signin.dart';
 // import 'package:first_project/pages/user_profile.dart';
 import 'package:get/get.dart';
 void main(){
-  runApp(const counter());
+  runApp(const Traveler());
 }
 
-class counter extends StatelessWidget {
-  const counter({super.key});
+class Traveler extends StatelessWidget{
+  const Traveler({super.key});
 
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-//      routes: {
-//        './homepage': (context) => homepage(isLoggedIn: true, username:""), this shit not neede
-//        './signup': (context) => signup(),
-//       './signin': (context) => signin(),
-//        './user_profile': (context) => user_profile(),
+      //scrollBehavior: MyCustomScrollBehavior(),
       debugShowCheckedModeBanner: false,
       home: homepage(isLoggedIn: true, username:"demo"),
+      
     );
   }
 }
+
+// class MyCustomScrollBehavior extends MaterialScrollBehavior {
+//   @override
+//   Set<PointerDeviceKind> get dragDevices => {
+//         PointerDeviceKind.touch,
+//         PointerDeviceKind.mouse, // Enables scrolling via mouse drag
+//       };
+// }
